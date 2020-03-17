ebb454cd212bf05496a339dd3bf0fef5df31278e
Dmitry Averyanov <dimthomas1@mail.ru>
2020-03-17 21:12:31 +0300

    Добавлена модель пользователя и функционал логина

--- webapp/config.py
+++ webapp/config.py
@@ -3,4 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db') 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
+
+SECRET_KEY = "HJHJ&*&HKhkad989KL?>/0024ljds8"
