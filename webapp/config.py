798763a5e6aa1e20e6b4c9399036184244fdabd8
Dmitry Averyanov <dimthomas1@mail.ru>
2020-04-04 09:33:28 +0300

    Flask-Migrations и регистрация пользователей

--- webapp/config.py
+++ webapp/config.py
@@ -9,3 +9,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db'
 SECRET_KEY = "HJHJ&*&HKhkad989KL?>/0024ljds8"
 
 REMEMBER_COOKIE_DURATION = timedelta(days=5)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
